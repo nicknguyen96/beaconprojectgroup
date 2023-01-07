6db65d61a56679867b3481d051016f48186b5fe6
Cruz Cordero Leon <105164264+CorderoCruz@users.noreply.github.com>
2023-01-07 14:30:32 -0500

    random commit

---
+++ client/beacongroupproject/src/app/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable, tap, catchError } from 'rxjs';
+import { BACKEND_URL } from '../utils/utils';
+import { Store } from '@ngrx/store';
+import { AuthActions } from '../store/user/auth.actions';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient, private store: Store) { }
+
+  login(email: string, password: string) {
+    return this.http.post(`${BACKEND_URL}/auth/login`, {email, password}).pipe(
+      tap((response: any) => {
+        const stringifiedToken: string = JSON.stringify(response.token)
+        const stringfiedEmployee: string = JSON.stringify(response.employee)
+        localStorage.setItem('token', stringifiedToken)
+        localStorage.setItem('employee', stringfiedEmployee)
+        this.store.dispatch(AuthActions.loginSuccess(response)), catchError(response => 
+        response.status)
+      }))}
+}
