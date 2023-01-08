3f0376d094dac4b38f363e390934addc2321c8e3
nhat nguyen <nmn009@ucsd.edu>
2023-01-08 14:33:07 -0800

    restructure send email function and complete updateFileStatus function

--- server/controllers/hrController.js
+++ server/controllers/hrController.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const { Employee } = require("../models");
 const { EmployeeDetail, LegalStatus, WorkStatus, FileUpload } = require('../models/EmployeeDetail');
+const sendEmail = require('../utils/sendEmail');
 
 class HrController {
   async sendInvitation(req, res) {
@@ -25,37 +26,26 @@ class HrController {
 
     const link = `${process.env.FRONTEND_URL}?token=${token}`;
     console.log(link);
-
     if (isHR) {
-      const options = {
-        from: process.env.email,
-        to: email,
-        subject: "Sending email with nodejs",
-        html: `
-                        <header>
-                            Hello ${email}! Welcome to my project management app!
-                        </header>
-                        <main>
-                            <p>Please click <a href=${link}>here</a> to sign up your account.</p>
-                            <p>This link will expire in 3 hours.</p>
-                        </main>
-                    `,
-      };
-      const transporter = nodemailer.createTransport({
-        service: "hotmail",
-        auth: {
-          user: process.env.email,
-          pass: process.env.password,
-        },
-      });
-      transporter.sendMail(options, function (error, info) {
-        if (error) {
-          console.log(error);
-          return res.json({ status: 400, message: error.message });
-        } else {
-          res.json({ status: 200, message: info });
-        }
-      });
+
+      const subject = 'Invitation to registration';
+      const html = `
+        <header>
+            Hello ${email}! Welcome to my project management app!
+        </header>
+        <main>
+            <p>Please click <a href=${link}>here</a> to sign up your account.</p>
+            <p>This link will expire in 3 hours.</p>
+        </main>
+    `;
+
+      const response = await sendEmail(email, subject, html);
+      console.log(response);
+      if (response.status == 200) {
+        res.json({ status: 200, message: "Send email successfully" });
+      } else {
+        res.json({ status: 400, message: response.message });
+      }
     } else {
       return res.json({ status: 401, message: "You do not have permission to perform this function" });
     }
@@ -92,15 +82,15 @@ class HrController {
   }
 
   async updateOnBoardingStatus(req, res) {
-    const { onboardingStatus, employeeId, message } = req.body;
+    const { onboardingStatus, employeeid, message } = req.body;
 
     try {
       // checking if the id is valid first before we start updating the status
-      if (!employeeId) {
-        throw new Error("employeeId must be provided.");
+      if (!employeeid) {
+        throw new Error("employeeid must be provided.");
       }
 
-      const employee = await Employee.findById(employeeId).populate("user");
+      const employee = await Employee.findById(employeeid).populate("user");
 
       if (!employee) {
         return res.json({ status: 404, message: "No employee found" });
@@ -115,133 +105,77 @@ class HrController {
       );
 
       // send email to employee to notify them the changes
-      const options = {
-        from: process.env.email,
-        to: employee.email,
-        subject: "Changes from onboarding application status",
-        html: `
-                        <header>
-                            Hello ${employee.email}! There is a message for you.
-                        </header>
-                        <main>
-                            <p>${message}<p>
-                            <p>Login <a href=${process.env.BACKEND_URL}>into</a> your account now to see the changes.</p>
-                        </main>
-                    `,
-      };
-      const transporter = nodemailer.createTransport({
-        service: "hotmail",
-        auth: {
-          user: process.env.email,
-          pass: process.env.password,
-        },
-      });
-      transporter.sendMail(options, function (error, info) {
-        if (error) {
-          console.log(error);
-          throw new Error("Cannot send email. Please try again");
-        }
-      });
+      const html = `
+                      <header>
+                          Hello ${employee.email}! There is a message for you.
+                      </header>
+                      <main>
+                          <p>${message}<p>
+                          <p>Login <a href=${process.env.BACKEND_URL}>into</a> your account now to see the changes.</p>
+                      </main>
+                  `;
+      const subject = "Changes in your onBoading Application";
 
-      res.json({ status: 200, message: "Successfully updated employee onboarding status", data: newEmployeeDetail });
-    } catch (err) {
-      console.log(err);
-      return res.json({ status: 400, message: err.message });
+      const response = await sendEmail(employee.email, subject, html);
+
+      if (response.status == 200) {
+        res.json({ status: 200, message: "Successfully updated and send email" });
+      } else {
+        res.json({ status: 400, message: response.message });
+      }
+
+    } catch (error) {
+      console.log(error);
+      return res.json({ status: 500, message: error.message });
     }
   }
 
-    // approve certain file
-    async approveFile(req, res) {
-        try {
-            const { employeeId, fileName } = req.body;
-            const employee = await 
-                Employee
-                .findOne({'_id': employeeId})
-                .select('-password');
-            if(!employee) {
-                res.json({ status: 404, msg: 'Employee Doesn\'t Exist'});
-            } else {
-                await EmployeeDetail.findOneAndUpdate(
-                    {"_id": employee.user},
-                    {$set: {
-                      'legalStatus.workStatus.fileUpload.$[file].status': 'Approved',
-                      'legalStatus.workStatus.fileUpload.$[file].message': 'Approved',
-                    }},
-                    {arrayFilters: [{'file.fileName': fileName}]});
-                res.json({ status: 201, msg: 'File approved' });
+  // approve certain file
+  async updateFileStatus(req, res) {
+    try {
+      const { employeeid, fileName, message, status } = req.body;
+      const employee = await
+        Employee
+          .findById(employeeid)
+          .select('-password');
+      if (!employee) {
+        res.json({ status: 404, message: 'Employee Doesn\'t Exist' });
+      } else {
+        console.log("employee ", employee);
+        await EmployeeDetail.findOneAndUpdate(
+          { "_id": employee.user },
+          {
+            $set: {
+              'legalStatus.workStatus.fileUpload.$[file].status': status,
+              'legalStatus.workStatus.fileUpload.$[file].message': message,
             }
-        } catch(error) {
-            res.json({ status: 500, msg: error.message });
-        }
-    };
+          },
+          { arrayFilters: [{ 'file.fileName': fileName }] });
 
-    // when hr reject certain file
-    async rejectFile(req, res) {
-      try {
-        const { employeeId, fileName, message } = req.body;
-        const employee = await 
-            Employee
-            .findOne({'_id': employeeId})
-            .select('-password');
-        if(!employee) {
-            res.json({ status: 404, msg: 'Employee Doesn\'t Exist'});
+        const subject = "New status in file uploaded";
+        const html = `
+                        <header>
+                            Hello ${employee.email}! This is a remainder for missing file
+                        </header>
+                        <main>
+                            <p>Please login Portal and upload the required file</p>
+                            <p>Thank you for your corporation.</p>
+                        </main>
+                    `;
+
+        const response = await sendEmail(employee.email, subject, html);
+
+        if (response.status == 200) {
+          res.json({ status: 200, message: `Update file ${fileName} to be ${status}` });
         } else {
-            await EmployeeDetail.findOneAndUpdate(
-                {"_id": employee.user},
-                {$set: {
-                  'legalStatus.workStatus.fileUpload.$[file].status': 'Rejected',
-                  'legalStatus.workStatus.fileUpload.$[file].message': message,
-                }},
-                {arrayFilters: [{'file.fileName': fileName}]});
-            res.json({ status: 201, msg: 'File Rejected' });
+          res.json({ status: 400, message: response.message });
         }
-      } catch(error) {
-        res.json({ status: 500, msg: error.message });
       }
-    };
-    // when hr send notification to user
-    async sendNotification(req, res) {
-        try {
-            // get email
-            const { email } = req.body;
-            if (!email) {
-                return res.json({ status: 400, message: "Email is required" });
-            }
-
-            // set ups for email notification
-            const options = {
-                from: process.env.email,
-                to: email,
-                subject: "Sending email with nodejs",
-                html: `
-                                <header>
-                                    Hello ${email}! This is a remainder for missing file
-                                </header>
-                                <main>
-                                    <p>Please login Portal and upload the required file</p>
-                                    <p>Thank you for your corporation.</p>
-                                </main>
-                            `,
-            };
-            const transporter = nodemailer.createTransport({
-                service: "hotmail",
-                auth: {
-                    user: process.env.email,
-                    pass: process.env.password,
-                }
-            });
-            transporter.sendMail(options, function (error, info) {
-                if (error) {
-                  console.log(error);
-                  return res.json({ status: 400, message: error.message });
-                } else {
-                  res.json({ status: 200, message: info });
-                }
-              });
-        } catch(error) {
-            res.json({ status: 500, msg: error.message });
-        }
+    } catch (error) {
+      console.log(error);
+      res.json({ status: 500, message: error.message });
     }
+  };
 }
 
 module.exports = new HrController();
--- server/routes/hrRoute.js
+++ server/routes/hrRoute.js
@@ -8,16 +8,9 @@ hrRouter.post("/sendInvitation", hrController.sendInvitation);
 // sending all the users to an admin
 hrRouter.get("/sortUser", hrController.sendSortedUsers);
 
-// when HR approves certain file
-hrRouter.post("/approveFile", hrController.approveFile);
+// when HR update file status certain file
+hrRouter.put("/updateFileStatus", hrController.updateFileStatus);
 
-// when HR reject certain file
-hrRouter.post("/rejectFile", hrController.rejectFile);
-
-// when HR send notification for certain file
-hrRouter.post("/notification", hrController.sendNotification);
-
-module.exports = hrRouter;
 hrRouter.put("/update-boarding-status", hrController.updateOnBoardingStatus);
 
 module.exports = hrRouter;
