d9cff2da05e10715f103b482e981590d1738b22a
nhat nguyen <nmn009@ucsd.edu>
2023-01-08 19:21:41 -0800

    finish hr middleware and logout

--- server/middlewares/isHR.js
+++ server/middlewares/isHR.js
@@ -1,6 +1,4 @@
 const jwt = require("jsonwebtoken");
-const path = require('path');
-require('dotenv').config({path: path.join(__dirname, '../.env')});
 const { JWT_SECRET } = process.env;
 const { BlackListToken } = require('../models/index');
 
@@ -9,14 +7,14 @@ module.exports = async function isHR(req, res, next) {
   const token = req.rawHeaders[1].split(' ')[1];
   const user = jwt.decode(token, JWT_SECRET);
   const inBlackList = await BlackListToken.findOne({token});
-  
   if (user.isHR && (Date.now()/1000 < user.exp) && !inBlackList) {
     req.headers["isHR"] = "true";
     req.headers["userid"] = user.userid;
-    req.headers["email"] = user.email;
   } else if ((Date.now()/1000 > user.exp) || inBlackList) {
     return res.json({ status: 403, message: "Invalid Token"})
-  }
+  } else if (!user.isHR){
+    return res.json({ status: 403, message: "Unauthorized Employee"})
+  } 
 
   return next();
 };
