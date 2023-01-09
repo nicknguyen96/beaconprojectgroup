48d28f2d5cfdb9fe276db33d99fb5bba39d6ab26
nhat nguyen <nmn009@ucsd.edu>
2023-01-09 09:32:00 -0800

    fix the checkRegToken minor error

--- server/middlewares/checkRegToken.js
+++ server/middlewares/checkRegToken.js
@@ -1,22 +1,19 @@
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = process.env;
-const { BlackListToken } = require('../models/index');
 
 module.exports = async (req, res, next) => {
     try {
         const regToken = req.headers.authorization.split(" ")[1];
         const decodedToken = jwt.verify(regToken, process.env.JWT_SECRET_INVITATION);
-        const inBlackList = await BlackListToken.findOne({token});
-        if (Date.now()/1000 > decodedToken.exp || inBlackList){
-            return res.json({status: 403, message: 'Your jwt is expired. Please login again'});
+        if (Date.now() / 1000 > decodedToken.exp) {
+            return res.json({ status: 403, message: 'Your jwt is expired. Please login again' });
         } else {
             const { email } = decodedToken;
             req.headers["email"] = email;
             next();
         }
     }
-    catch (err) {
+    catch (error) {
         console.log('you do not have a reg token.');
-        res.json({status: 401, message: message.error})
+        res.json({ status: 401, message: error.message })
     }
 };
