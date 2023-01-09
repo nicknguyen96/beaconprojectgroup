53279ced82d3cc65ce33860caca4c9c702237a9f
YK Dong <ykdong1991@gmail.com>
2023-01-08 21:34:56 -0500

    finish isHR middleware

--- server/middlewares/isHR.js
+++ server/middlewares/isHR.js
@@ -1,7 +1,18 @@
 const jwt = require("jsonwebtoken");
+const { BlackListToken } = require('../models/index');
 
-module.exports = function isHR(req, res, next) {
+module.exports = async function isHR(req, res, next) {
+
+  const token = req.rawHeaders[1].split(' ')[1];
+  const user = jwt.decode(token);
+  const inBlackList = await BlackListToken.findOne({token});
   
-  req.headers["isHR"] = "true";
+  if (user.isHR && (Date.now()/1000 < user.exp) && !inBlackList) {
+    req.headers["isHR"] = "true";
+    req.headers["userid"] = user.userid;
+  } else if ((Date.now()/1000 > user.exp) || inBlackList) {
+    return res.json({ status: 403, message: "Invalid Token"})
+  }
+  
   return next();
 };
