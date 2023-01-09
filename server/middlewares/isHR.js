360d631b3d6441eedfb9e331a745a81d8996f522
YK Dong <ykdong1991@gmail.com>
2023-01-08 21:43:28 -0500

    modify isHR middleware

--- server/middlewares/isHR.js
+++ server/middlewares/isHR.js
@@ -1,10 +1,13 @@
 const jwt = require("jsonwebtoken");
+const path = require('path');
+require('dotenv').config({path: path.join(__dirname, '../.env')});
+const { JWT_SECRET } = process.env;
 const { BlackListToken } = require('../models/index');
 
 module.exports = async function isHR(req, res, next) {
 
   const token = req.rawHeaders[1].split(' ')[1];
-  const user = jwt.decode(token);
+  const user = jwt.decode(token, JWT_SECRET);
   const inBlackList = await BlackListToken.findOne({token});
   
   if (user.isHR && (Date.now()/1000 < user.exp) && !inBlackList) {
@@ -13,6 +16,6 @@ module.exports = async function isHR(req, res, next) {
   } else if ((Date.now()/1000 > user.exp) || inBlackList) {
     return res.json({ status: 403, message: "Invalid Token"})
   }
-  
+
   return next();
 };
