5af590ad3ed3e2e29ef71a8af83a6a9691effd3b
Nhat Nguyen <56015500+nicknguyen96@users.noreply.github.com>
2023-01-10 09:02:55 -0800

    Update isHR.js

--- server/middlewares/isHR.js
+++ server/middlewares/isHR.js
@@ -8,7 +8,7 @@ module.exports = async function isHR(req, res, next) {
     const token = req.headers.authorization.split(" ")[1];
     if (!token) return res.json({status: 400, message: "must provide valid authorization header"});
     const user = jwt.decode(token, JWT_SECRET);
-    console.log(user);
+    if (!user) return res.json({ status: 403, message: "Invalid Token" })
     const inBlackList = await BlackListToken.findOne({ token });
     if (user && user.isHR && (Date.now() / 1000 < user.exp) && !inBlackList) {
       req.headers["isHR"] = "true";
